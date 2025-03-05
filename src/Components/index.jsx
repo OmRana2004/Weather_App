cd14102c93a55d8b84a46f9fbf6fb6cccfd51951
om rana <omrana8755800322@gmail.com>
2025-03-05 19:42:27 +0530

    ok

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -1,3 +1,4 @@
 export { default as BackgroundLayoute } from './BackgroundLayoute'
 export { default as MiniCard } from './MiniCard'
 export { default as WeatherCard } from './WeatherCard'
+// check
